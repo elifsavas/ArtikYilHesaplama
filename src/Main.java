7e2f8a47028ed0add0ebf95affd2168e8c366f05
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-28 14:18:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen Yıl Giriniz:");
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if ((year %400 !=0) && (year %100 == 0)) {
+                System.out.println(year + " Artık yıl değildir!");
+            } else {
+                System.out.println(year + " Artık yıldır!");
+            }
+        } else {
+            System.out.println(year + " Artık yıl değildir!");
+        }
+
+
     }
 }
